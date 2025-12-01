f48c7d51daaabb2f02099e55bdbcd7be100acce3
Hritik Chauhan <hritikchji@gmail.com>
2025-12-01 19:15:31 +0530

    add validation middleware for request body schema validation

---
+++ src/middlewares/middleware.validate.js
@@ -0,0 +1,9 @@
+export default function validate(schema) {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ status: "error", message: error.message });
+    }
+    next();
+  };
+}
