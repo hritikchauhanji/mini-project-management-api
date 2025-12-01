fdd91b195a0b0894936e131083329d613522f86e
Hritik Chauhan <hritikchji@gmail.com>
2025-12-01 19:20:22 +0530

    add JWT authentication middleware and update environment sample

--- src/middlewares/validate.js
+++
@@ -1,9 +0,0 @@
-export default function validate(schema) {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body);
-    if (error) {
-      return res.status(400).json({ status: "error", message: error.message });
-    }
-    next();
-  };
-}
